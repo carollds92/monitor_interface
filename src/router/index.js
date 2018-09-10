2ab79a479286c47bac290299d65886714518181a
Luan Andryl <luan.andryl@gmail.com>
2018-09-10 18:06:21 -0300

    action components routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Login from '@/components/Auth/Login'
 import Singup from '@/components/Auth/Singup'
 import Dashboard from '@/components/dashboard/Dashboard'
+import Charts from '@/components/dashboard/Charts'
+import NewPatient from '@/components/dashboard/NewPatient'
+import Profile from '@/components/dashboard/Profile'
 
 Vue.use(Router)
 
@@ -12,6 +16,24 @@ export default new Router({
       path: '/',
       name: 'dashboard',
       component: Dashboard,
+      meta: {requiresAuth: true}
+    },
+    {
+      path: '/charts',
+      name: 'charts',
+      component: Charts,
+      meta: {requiresAuth: true}
+    },
+    {
+      path: '/patient',
+      name: 'patient',
+      component: NewPatient,
+      meta: {requiresAuth: true}
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: Profile,
       meta: {requiresAuth: true}
     },
     {
