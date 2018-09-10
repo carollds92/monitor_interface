4d8075e33a9023ba287891049f2b96cc5d2977a3
Luan Andryl <luan.andryl@gmail.com>
2018-09-10 18:04:40 -0300

    chart bootstrap component

---
+++ src/components/dashboard/Charts.vue
@@ -0,0 +1,23 @@
+<script>
+import Navigation from './Navigation.vue'
+export default {
+  name: 'charts',
+  components: {
+    Navigation
+  }
+}
+</script>
+<template>
+  <div class="wrapper">
+    <Navigation></Navigation>
+    <h1>Graficos</h1>
+  </div>
+</template>
+<style lang="scss" scoped>
+$main-bg: #ecf0f1;
+
+.wrapper{
+  background: $main-bg;
+  height: 100vh;
+}
+</style>
