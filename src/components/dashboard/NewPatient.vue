6d3ff71340098a71616e0304be411ccd153bf0cc
Luan Andryl <luan.andryl@gmail.com>
2018-09-10 18:05:26 -0300

    newPatient bootstrap component

---
+++ src/components/dashboard/NewPatient.vue
@@ -0,0 +1,23 @@
+<script>
+import Navigation from './Navigation.vue'
+export default {
+  name: 'newPatient',
+  components: {
+    Navigation
+  }
+}
+</script>
+<template>
+  <div class="wrapper">
+    <Navigation></Navigation>
+    <h1>Cadastro Paciente</h1>
+  </div>
+</template>
+<style lang="scss" scoped>
+$main-bg: #ecf0f1;
+
+.wrapper{
+  background: $main-bg;
+  height: 100vh;
+}
+</style>
