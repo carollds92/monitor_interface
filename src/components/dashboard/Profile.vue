bd6c847bb5097bac12767f7b6a615c6db1038014
Luan Andryl <luan.andryl@gmail.com>
2018-09-10 18:05:37 -0300

    profile boostrap component

---
+++ src/components/dashboard/Profile.vue
@@ -0,0 +1,23 @@
+<script>
+import Navigation from './Navigation.vue'
+export default {
+  name: 'profile',
+  components: {
+    Navigation
+  }
+}
+</script>
+<template>
+  <div class="wrapper">
+    <Navigation></Navigation>
+    <h1>Perfil</h1>
+  </div>
+</template>
+<style lang="scss" scoped>
+$main-bg: #ecf0f1;
+
+.wrapper{
+  background: $main-bg;
+  height: 100vh;
+}
+</style>
