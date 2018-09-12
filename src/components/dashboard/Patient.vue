0f6ad028a839898ee36f4f707cbfdb41b4d43f43
Luan Andryl <luan.andryl@gmail.com>
2018-09-12 03:34:45 -0300

    patient edit

--- src/components/dashboard/Patient.vue
+++ src/components/dashboard/Patient.vue
@@ -11,7 +11,16 @@ export default {
   },
   data () {
     return {
-      patientList: null
+      patientList: null,
+      patientEdit: {
+        _id: '',
+        first_name: '',
+        last_name: '',
+        email: '',
+        age: '',
+        weight: '',
+        medicines: ''
+      }
     }
   },
   methods: {
@@ -29,10 +38,56 @@ export default {
           let API_TOKEN = { headers: {'Authorization': JSON.parse(localStorage.getItem('auth')).token.type_token + ' ' + JSON.parse(localStorage.getItem('auth')).token.acess_token} }
           axios.delete(API_URL + 'patient/' + id, API_TOKEN).then(response => {
             if (response.status === 200) {
-              this.patientList = this.patientList.filter(item => item._id !== response.data._id)
+              axios.get(API_URL + 'patient/', API_TOKEN).then(response => { this.patientList = response.data })
               this.$swal(
                 'Excluido!',
                 'O paciente foi removido da base de Dados.',
+                'success'
+              )
+            }
+          }).catch(error => {
+            console.log(error.response.data)
+          })
+        }
+      })
+    },
+    openDialog (ref) {
+      this.$refs[ref].open()
+    },
+    closeDialog (ref) {
+      this.$refs[ref].close()
+    },
+    editClickHandller: function (id) {
+      let el = this.patientList.find(item => {
+        return item._id === id
+      })
+      this.patientEdit._id = el._id
+      this.patientEdit.first_name = el.first_name
+      this.patientEdit.last_name = el.last_name
+      this.patientEdit.email = el.email
+      this.patientEdit.age = el.age
+      this.patientEdit.weight = el.weight
+      this.patientEdit.medicines = el.medicines
+      this.openDialog('modal_edit')
+    },
+    saveEdited () {
+      this.$swal({
+        title: 'Você tem certeza?',
+        text: 'Você não poderá reverter esta ação!',
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim, salvar alteraçoẽs!'
+      }).then((result) => {
+        if (result.value) {
+          let API_TOKEN = { headers: {'Authorization': JSON.parse(localStorage.getItem('auth')).token.type_token + ' ' + JSON.parse(localStorage.getItem('auth')).token.acess_token} }
+          axios.put(API_URL + 'patient/' + this.patientEdit._id, this.patientEdit, API_TOKEN).then(response => {
+            if (response.status === 200) {
+              axios.get(API_URL + 'patient/', API_TOKEN).then(response => { this.patientList = response.data })
+              this.$swal(
+                'Sucesso!',
+                'O paciente foi editado.',
                 'success'
               )
             }
@@ -59,11 +114,60 @@ export default {
   <div class="wrapper">
     <Navigation></Navigation>
 
+    <md-dialog  ref="modal_edit">
+      <md-dialog-title>Editar Paciente</md-dialog-title>
+        
+        <div class="wrapper-form">
+          <div class="box">
+            <div class="form">
+
+              <md-input-container>
+                <label>Nome</label>
+                <md-input v-model="patientEdit.first_name" type="name"></md-input>
+              </md-input-container>
+
+              <md-input-container>
+                <label>Sobrenome</label>
+                <md-input v-model="patientEdit.last_name" type="name"></md-input>
+              </md-input-container>
+              
+              <md-input-container>
+                <label>Email</label>
+                <md-input v-model="patientEdit.email" type="email"></md-input>
+              </md-input-container>
+
+              <md-input-container>
+                <label>Idade</label>
+                <md-input v-model="patientEdit.age" type="text"></md-input>
+              </md-input-container>
+
+              <md-input-container>
+                <label>Peso</label>
+                <md-input v-model="patientEdit.weight" type="text"></md-input>
+              </md-input-container>
+
+              <md-input-container>
+                <label>Medicamentos</label>
+                <md-textarea v-model="patientEdit.medicines"></md-textarea>
+              </md-input-container>
+
+            </div>
+          </div>
+        </div>
+
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="closeDialog('modal_edit')">Fechar</md-button>
+        <md-button class="md-primary" @click="saveEdited()">Salvar</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+
     <div class="table-wrapper">
       <md-table class="table-color" md-card>
-        <md-table-toolbar>
+        <md-toolbar>
           <h1 class="md-title">Pacientes</h1>
-        </md-table-toolbar>
+          <md-button class="md-icon-button md-primary" href="#/new-patient"><md-icon >add_circle</md-icon></md-button>
+        </md-toolbar>
+        
         <md-table-row>
           <md-table-head >Nome</md-table-head>
           <md-table-head>Sobrenome</md-table-head>
@@ -72,6 +176,7 @@ export default {
           <md-table-head>Peso</md-table-head>
           <md-table-head>Medicamentos</md-table-head>
           <md-table-head>Remover</md-table-head>
+          <md-table-head>Editar</md-table-head>
         </md-table-row>
 
         <md-table-row v-for="patient in patientList" :key="patient._id">
@@ -82,19 +187,40 @@ export default {
           <md-table-cell md-label="weight">{{patient.weight}}</md-table-cell>
           <md-table-cell md-label="">{{patient.medicines}}</md-table-cell>
           <md-table-cell><md-button class="md-icon-button md-primary" v-on:click="removeHandller(patient._id)"><md-icon >clear</md-icon></md-button></md-table-cell>
+          <md-table-cell><md-button class="md-icon-button md-primary" v-on:click="editClickHandller(patient._id)"><md-icon >create</md-icon></md-button></md-table-cell>
+
         </md-table-row>
       </md-table>
-      <div class="controls">
-        <md-button class="md-raised md-primary" href="#/new-patient">Cadastrar Pacientes</md-button>
-      </div>
+      
     </div>
   </div>
 </template>
 <style lang="sass" scoped>
 $main-bg: #ecf0f1
 
+.form 
+  width: 450px
+  font-family: 'Quicksand', sans-serif
+  font-weight: 200
+
+.wrapper-form 
+  height: 100vh
+  display: flex
+  flex-flow: column wrap
+  justify-content: center
+  align-items: center
+
+
+.box
+  padding: 5rem
+  border-radius: 15px
+  display: flex
+  flex-flow: column wrap
+  justify-content: center
+  align-items: center
+
+
 .controls 
-  margin-top: 4rem
   display: flex
   flex-flow: column wrap
   text-align: center
@@ -116,4 +242,7 @@ $main-bg: #ecf0f1
   padding: 2.5rem
   border-radius: 20px
 
+.md-theme-default.md-toolbar
+  color: #000
+  background: #fff
 </style>
