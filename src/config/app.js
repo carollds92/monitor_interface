73a0990353cc4efc0e15562e85cac8131e0a1dd3
Luan Andryl <luan.andryl@gmail.com>
2018-09-11 22:21:57 -0300

    get_token config

--- src/config/app.js
+++ src/config/app.js
@@ -6,6 +6,12 @@ export default {
   */
   // Returns the test api url
   getApiURL () {
+    // return 'https://monitorv1-api.herokuapp.com/'
     return 'http://localhost:3000/'
+  },
+  getApiToken () {
+    return {
+      headers: {'Authorization': JSON.parse(localStorage.getItem('auth')).token.type_token + ' ' + JSON.parse(localStorage.getItem('auth')).token.acess_token}
+    }
   }
 }
