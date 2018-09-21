cb89087d57bb1f7db2eb816ae0cf180cb6f37c42
Luan Andryl <luan.andryl@gmail.com>
2018-09-21 13:17:16 -0300

    session time expired logout

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import router from './router'
 import VueMaterial from 'vue-material'
 import VueSweetalert2 from 'vue-sweetalert2'
 import VueGoogleCharts from 'vue-google-charts'
+import axios from 'axios'
+import config from './config/app'
+const API_URL = config.getApiURL()
 
 import 'vue-material/dist/vue-material.css'
 import Auth from './services/Authentication'
@@ -22,6 +25,13 @@ Vue.material.registerTheme('default', {
     hue: '200'
   }
 })
+
+try {
+  let API_TOKEN = { headers: {'Authorization': JSON.parse(localStorage.getItem('auth')).token.type_token + ' ' + JSON.parse(localStorage.getItem('auth')).token.acess_token} }
+  axios.get(API_URL + 'patient/', API_TOKEN).then()
+} catch (error) {
+  Auth.logout()
+}
 
 router.beforeEach((to, from, next) => {
   if (to.meta.requiresAuth) {
