74a32c42571fa8ed82e8b0361b7c30e32e46f3b0
Luan <luan.andryl@gmail.com>
2018-10-02 02:30:59 -0300

    plot chart from sensor data

--- src/components/dashboard/Charts.vue
+++ src/components/dashboard/Charts.vue
@@ -13,22 +13,13 @@ export default {
   },
   data () {
     return {
-      chartData: [
-        ['Day', 'Bpm Avg'],
-        ['Mon', 100],
-        ['Tue', 117],
-        ['Wed', 96],
-        ['Thu', 103],
-        ['Fri', 90],
-        ['Sat', 89],
-        ['Mon', 85]
-      ],
+      chartData: null,
       chartOptions: {
         chart: {
           title: 'Avg of BPM',
           subtitle: 'Avg of BPM for Patient'
         },
-        width: 600,
+        width: 900,
         height: 300
       },
       patientList: []
@@ -47,15 +38,38 @@ export default {
     })
   },
   methods: {
-    chartHandller: function (id) {
-      console.log(id)
+    chartHandller: function (sensor) {
+      let API_TOKEN = { headers: {'Authorization': JSON.parse(localStorage.getItem('auth')).token.type_token + ' ' + JSON.parse(localStorage.getItem('auth')).token.acess_token} }
+      axios.get(API_URL + 'patient/monitor/' + sensor, API_TOKEN).then(response => {
+        if (response.data) {
+          this.chartData = [['N', 'BPM']]
+          response.data.forEach((e, i) => {
+            this.chartData.push([i, e.bpm])
+          })
+          console.log(this.chartData)
+        }
+      }).catch(error => {
+        console.log(error.response)
+      })
+
       this.openDialog('modal_patient')
     },
     openDialog (ref) {
-      this.$refs[ref].open()
+      if (this.chartData !== null) {
+        this.$refs[ref].open()
+      } else {
+        this.$swal(
+              'Ops!',
+              'O paciente ainda não tem dados coletados.',
+              'warning'
+            )
+      }
     },
     closeDialog (ref) {
       this.$refs[ref].close()
+    },
+    clearChartData () {
+      this.chartData = null
     }
   }
 }
@@ -75,6 +89,7 @@ export default {
           <md-table-head>Email</md-table-head>
           <md-table-head>Idade</md-table-head>
           <md-table-head>Peso</md-table-head>
+          <md-table-head>Monitor ID</md-table-head>
           <md-table-head>Medicamentos</md-table-head>
           <md-table-head>Grafico de BPM</md-table-head>
         </md-table-row>
@@ -85,13 +100,14 @@ export default {
           <md-table-cell md-label="email" md-sort-by="email">{{patient.email}}</md-table-cell>
           <md-table-cell md-label="age">{{patient.age}}</md-table-cell>
           <md-table-cell md-label="weight">{{patient.weight}}</md-table-cell>
+          <md-table-cell md-label="sensor">{{patient.sensor}}</md-table-cell>
           <md-table-cell md-label="">{{patient.medicines}}</md-table-cell>
-          <md-table-cell><md-button class="md-icon-button md-primary" v-on:click="chartHandller(patient._id)"><md-icon >bar_chart</md-icon></md-button></md-table-cell>
+          <md-table-cell><md-button class="md-icon-button md-primary" v-on:click="chartHandller(patient.sensor)"><md-icon >bar_chart</md-icon></md-button></md-table-cell>
         </md-table-row>
       </md-table>
     </div>
     
-    <md-dialog  ref="modal_patient">
+    <md-dialog @close="clearChartData" ref="modal_patient">
       <md-dialog-title>Gráfico de BPM</md-dialog-title>
       <div class="chart-wrapper">
         <GChart
